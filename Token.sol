4cf4e53e5b4e0110ce637d352669e3741bd725a5
Erin Beatrice Micaela G. Reyes <89332224+YumenoRetort@users.noreply.github.com>
2024-07-25 22:20:11 +0800

    Update Token.sol

--- Token.sol
+++ Token.sol
@@ -5,7 +5,7 @@ interface IERC20 {
     function totalSupply() external view returns (uint256);
     function balanceOf(address account) external view returns (uint256);
     function transfer(address recipient, uint256 amount) external returns (bool);
-    function allowance(address owner, address spender) external view returns (uint256);
+    function allowance(address tokenOwner, address spender) external view returns (uint256);
     function approve(address spender, uint256 amount) external returns (bool);
     function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
 
@@ -18,15 +18,22 @@ contract ERC20Token is IERC20 {
     string public symbol;
     uint8 public decimals;
     uint256 private _totalSupply;
+    address public owner;
     mapping(address => uint256) private _balances;
     mapping(address => mapping(address => uint256)) private _allowances;
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only the contract owner can call this function");
+        _;
+    }
 
     constructor(string memory tokenName, string memory tokenSymbol, uint8 tokenDecimals, uint256 initialSupply) {
         name = tokenName;
         symbol = tokenSymbol;
         decimals = tokenDecimals;
-        _totalSupply = initialSupply * 10 ** uint256(decimals);
+        _totalSupply = initialSupply;
         _balances[msg.sender] = _totalSupply;
+        owner = msg.sender;
         emit Transfer(address(0), msg.sender, _totalSupply);
     }
 
@@ -43,8 +50,8 @@ contract ERC20Token is IERC20 {
         return true;
     }
 
-    function allowance(address owner, address spender) external view override returns (uint256) {
-        return _allowances[owner][spender];
+    function allowance(address tokenOwner, address spender) external view override returns (uint256) {
+        return _allowances[tokenOwner][spender];
     }
 
     function approve(address spender, uint256 amount) external override returns (bool) {
@@ -58,6 +65,20 @@ contract ERC20Token is IERC20 {
         return true;
     }
 
+    function mint(address account, uint256 amount) external onlyOwner {
+        require(account != address(0), "ERC20: mint to the zero address");
+        _totalSupply += amount;
+        _balances[account] += amount;
+        emit Transfer(address(0), account, amount);
+    }
+
+    function burn(uint256 amount) external {
+        require(_balances[msg.sender] >= amount, "ERC20: burn amount exceeds balance");
+        _balances[msg.sender] -= amount;
+        _totalSupply -= amount;
+        emit Transfer(msg.sender, address(0), amount);
+    }
+
     function _transfer(address sender, address recipient, uint256 amount) internal {
         require(sender != address(0), "ERC20: transfer from the zero address");
         require(recipient != address(0), "ERC20: transfer to the zero address");
@@ -68,11 +89,11 @@ contract ERC20Token is IERC20 {
         emit Transfer(sender, recipient, amount);
     }
 
-    function _approve(address owner, address spender, uint256 amount) internal {
-        require(owner != address(0), "ERC20: approve from the zero address");
+    function _approve(address tokenOwner, address spender, uint256 amount) internal {
+        require(tokenOwner != address(0), "ERC20: approve from the zero address");
         require(spender != address(0), "ERC20: approve to the zero address");
 
-        _allowances[owner][spender] = amount;
-        emit Approval(owner, spender, amount);
+        _allowances[tokenOwner][spender] = amount;
+        emit Approval(tokenOwner, spender, amount);
     }
 }
